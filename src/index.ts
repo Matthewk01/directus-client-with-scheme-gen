298f4b25ec347fef4c2b2ed80341a27a644712bf
matej <matej@yolkstudio.com>
2024-02-02 15:43:51 +0100

    feat: update gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import {authentication, createDirectus, readItems, rest} from "@directus/sdk";
-import {Schema} from "node:inspector";
+import { authentication, createDirectus, readItems, rest } from "@directus/sdk";
+import { Schema } from "../types/directus";
 
-const client = createDirectus<Schema>('http://cms.modular.altron.net').with(authentication()).with(rest());
+const client = createDirectus<Schema>("http://cms.modular.altron.net")
+  .with(authentication())
+  .with(rest());
 
 (async () => {
-        await client.login('cmsadmin@modular.altron.net', '7h9g58v74eyj872')
-        const result = await client.request(readItems('article_category', { limit: 3 }))
-        for (const item of result) {
-            console.log(item.id, item.slug);
-        }
-    }
-)();
+  await client.login("cmsadmin@modular.altron.net", "7h9g58v74eyj872");
+  const result = await client.request(
+    readItems("article_category", { limit: 3 }),
+  );
+  for (const item of result) {
+    console.log(item.id, item.slug);
+  }
+})();
